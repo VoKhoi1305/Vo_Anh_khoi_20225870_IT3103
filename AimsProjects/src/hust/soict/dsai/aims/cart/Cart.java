5827aec5f03f15fec1c1d6142da93c94cf64a430
Vo Anh Khoi <voanhkhoi91b@gmail.com>
2024-11-25 18:25:43 +0700

    refactor + garbage

---
+++ AimsProjects/src/hust/soict/dsai/aims/cart/Cart.java
@@ -0,0 +1,101 @@
+package hust.soict.dsai.aims.cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Cart {
+	public static final int MAX_NUMBER_OEDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_OEDERED];
+	private int totalItems;
+	
+	 public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+	        if (totalItems < MAX_NUMBER_OEDERED) {
+	            itemsOrdered[totalItems] = disc; 
+	            totalItems++; 
+	 }
+	 }
+	 public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+		    if (totalItems + dvdList.length <= MAX_NUMBER_OEDERED) {
+		        for (int i = 0; i < dvdList.length; i++) {
+		            itemsOrdered[totalItems] = dvdList[i]; 
+		            totalItems++; 
+		        }
+		    } else {
+		        System.out.println("The cart is full.");
+		    }
+		}
+	
+	 public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
+	    if (totalItems + dvds.length <= MAX_NUMBER_OEDERED) {
+	        for (int i = 0; i < dvds.length; i++) {
+	            itemsOrdered[totalItems] = dvds[i]; 
+	            totalItems++; 
+	        }
+	    } else {
+	        System.out.println("The cart is almost full. Cannot add all DVDs!");
+	    }
+	}
+	 
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean found = false;
+        for (int i = 0; i < totalItems; i++) {
+            if (itemsOrdered[i].getTitle().equals(disc.getTitle())) { // Check if the disc matches
+                found = true;
+                // Shift items to the left to remove the disc
+                for (int j = i; j < totalItems - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[totalItems - 1] = null; // Clear the last item
+                totalItems--; // Decrement the count of items
+                break;
+            }
+        }
+    }
+	
+	
+	
+	public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < totalItems; i++) {
+            total += itemsOrdered[i].getCost(); // Sum the costs of the DVDs
+        }
+        return total;
+    }
+	
+	
+	public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+
+        double totalCost = 0;
+        for (int i = 0; i < totalItems; i++) {
+            DigitalVideoDisc dvd = itemsOrdered[i];
+            System.out.println((i + 1) + ". " + dvd);
+            totalCost += dvd.getCost();
+        }
+        System.out.println("Total cost: " + totalCost + " $");
+        System.out.println("***************************************************");
+    }
+	
+	 public DigitalVideoDisc searchById(int id) {
+	        for (DigitalVideoDisc dvd : itemsOrdered) {
+	            if (dvd.getid() == id) {
+	                return dvd;
+	            }
+	        }
+	        return null; // Return null if not found
+	    }
+
+	    // Search DVDs by title
+	    public void searchByTitle(String title) {
+	        boolean found = false;
+	        for (DigitalVideoDisc dvd : itemsOrdered) {
+	            if (dvd.isMatch(title)) {
+	                System.out.println(dvd);
+	                found = true;
+	            }
+	        }
+
+	        if (!found) {
+	            System.out.println("No DVD found with the title: " + title);
+	        }
+	    }
+}
---
+++ AimsProjects/src/hust/soict/dsai/test/store/TestStore.java
@@ -0,0 +1,30 @@
+package hust.soict.dsai.test.store;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
+
+public class TestStore {
+        public static void main(String[] args) {
+            Store store = new Store(5);
+            DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Matrix", "Action", "Wachowski", 136, 19.99f);
+            DigitalVideoDisc dvd2 = new DigitalVideoDisc("Inception", "Sci-Fi", "Christopher Nolan", 148, 14.99f);
+            DigitalVideoDisc dvd3 = new DigitalVideoDisc("The Dark Knight", "Action", "Christopher Nolan", 152, 24.99f);
+            DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avengers: Endgame", "Action", "Anthony Russo", 181, 29.99f);
+            DigitalVideoDisc dvd5 = new DigitalVideoDisc("Interstellar", "Sci-Fi", "Christopher Nolan", 169, 19.99f);
+            store.addDVD(dvd1);
+            store.addDVD(dvd2);
+            store.addDVD(dvd3);
+            store.addDVD(dvd4);
+            store.addDVD(dvd5);
+            DigitalVideoDisc dvd6 = new DigitalVideoDisc("Avatar", "Sci-Fi", "James Cameron", 162, 22.99f);
+            store.addDVD(dvd6); 
+            store.displayItems();
+            System.out.println("\n Delete Inception");
+            store.removeDVD(dvd2);
+            store.displayItems();
+            System.out.println("\n Delete Titanic not exist");
+            DigitalVideoDisc dvd7 = new DigitalVideoDisc("Titanic", "Romance", "James Cameron", 195, 19.99f);
+            store.removeDVD(dvd7);
+            store.displayItems();
+        }
+}
