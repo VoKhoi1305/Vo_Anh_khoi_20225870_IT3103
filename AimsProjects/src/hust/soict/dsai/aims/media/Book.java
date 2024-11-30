f94637c2c6d815bcf9ad2a606dfd0ae0e81caf7e
Vo Anh Khoi <voanhkhoi91b@gmail.com>
2024-11-30 08:54:25 +0700

    book & media

--- AimsProjects/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProjects/src/hust/soict/dsai/aims/media/Book.java
@@ -1,13 +1,7 @@
 package hust.soict.dsai.aims.media;
-
-import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.List;
-public class Book {
-	private int id;
-	private String title;
-	private String category;
-	private float cost;
+public class Book extends Media {
 	private List<String> authors = new ArrayList<String>();
 	
 	public int getId() {
